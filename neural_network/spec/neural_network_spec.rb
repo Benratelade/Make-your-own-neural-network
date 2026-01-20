88b724c62d62ea665e56a39847e8546103c09a41
Ben Ratelade <ben@fresho.com>
2026-01-20 20:56:57 +1100

    Add a test to verify the activation function works

--- neural_network/spec/neural_network_spec.rb
+++ neural_network/spec/neural_network_spec.rb
@@ -4,17 +4,35 @@ require_relative '../neural_network'
 require 'matrix'
 
 describe NeuralNetwork do
-  it 'calculates the output of an input' do
-    neural_network = NeuralNetwork.new(
-      input_nodes_count: 3,
-      hidden_nodes_count: 3,
-      output_nodes_count: 3,
-      learning_rate: 0.2
-    )
+  describe '#activation_function' do
+    it 'applies the activation function to a matrix' do
+      neural_network = NeuralNetwork.new(
+        input_nodes_count: 3,
+        hidden_nodes_count: 3,
+        output_nodes_count: 3,
+        learning_rate: 0.2
+      )
 
-    expect(neural_network.query(input_list: [1, 2, 3])).to eq(
-      Matrix[[0.4999917331524737], [0.5930703309561164], [0.5690019973981506]]
-    )
+      matrix = Matrix[[1.05], [0.6]]
+
+      expect(neural_network.activation_function(matrix)[0, 0]).to be_within(0.0001).of(0.7408)
+      expect(neural_network.activation_function(matrix)[1, 0]).to be_within(0.0001).of(0.6457)
+    end
+  end
+
+  describe '#query' do
+    it 'calculates the output of an input' do
+      neural_network = NeuralNetwork.new(
+        input_nodes_count: 3,
+        hidden_nodes_count: 3,
+        output_nodes_count: 3,
+        learning_rate: 0.2
+      )
+
+      expect(neural_network.query(input_list: [1, 2, 3])).to eq(
+        Matrix[[0.4999917331524737], [0.5930703309561164], [0.5690019973981506]]
+      )
+    end
   end
 
   describe '#generate_starting_weights_for_network' do
